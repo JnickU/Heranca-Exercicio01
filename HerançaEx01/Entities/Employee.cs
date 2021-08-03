ba3b897ece2aaf39858526fa262dc3bc2e9d99dc
JnickU <nicknickeado1@gmail.com>
2021-08-03 00:02:26 -0300

    Implementação completa

---
+++ HerançaEx01/Entities/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Globalization;
+
+namespace Herança.Entities {
+    class Employee {
+        public string Name { get; protected set; }
+        public int Hours { get; protected set; }
+        public double ValuePerHour { get; protected set; }
+
+        public Employee() {
+
+        }
+
+        public Employee(string name, int hours, double valuePerHour) {
+            Name = name;
+            Hours = hours;
+            ValuePerHour = valuePerHour;
+        }
+
+        public virtual double Payment() {
+            return Hours * ValuePerHour;
+        }
+
+        public override string ToString() {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(Name + " - $ " + Payment().ToString("F2", CultureInfo.InvariantCulture));
+            return sb.ToString();
+        }
+    }
+}
